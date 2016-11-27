52e43d37cb4dd70404ae14d24806dff961b13d43
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2016-11-27 10:04:11 -0200

    Adicionar routes

--- full-stack-web-developer/3-front-end-javascript-frameworks-angularjs/1-confusion/gulpfile.js
+++ full-stack-web-developer/3-front-end-javascript-frameworks-angularjs/1-confusion/gulpfile.js
@@ -12,7 +12,8 @@ var gulp = require('gulp'),
 	changed = require('gulp-changed'),
 	rev = require('gulp-rev'),
 	browserSync = require('browser-sync'),
-	del = require('del');
+	del = require('del'),
+	ngannotate = require('gulp-ng-annotate');
 
 gulp.task('jshint', function() {
 	return gulp.src('app/scripts/**/*.js')
@@ -29,10 +30,10 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-	return gulp.src('./app/menu.html')
+	return gulp.src('./app/index.html')
 		.pipe(usemin({
 			css:[minifycss(),rev()],
-			js: [uglify(),rev()]
+			js: [ngannotate(),uglify(),rev()]
 		}))
 		.pipe(gulp.dest('dist/'));
 });
@@ -73,7 +74,7 @@ gulp.task('browser-sync', ['default'], function () {
 	browserSync.init(files, {
 		server: {
 			baseDir: "dist",
-			index: "menu.html"
+			index: "index.html"
 		}
 	});
 	// Watch any files in dist/, reload on change
